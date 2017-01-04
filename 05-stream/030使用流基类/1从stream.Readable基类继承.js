17137f0f25f182b467e09df4ebf04ec488865625
fridolph <249121486@qq.com>
2017-01-04 15:50:19 +0800

    流模块的学习~~

--- 05-stream/030使用流基类/1从stream.Readable基类继承.js
+++ 05-stream/030使用流基类/1从stream.Readable基类继承.js
@@ -9,3 +9,13 @@ function MyStream(options) {
 MyStream.prototype = Object.craete(Readable.prototype, {
   constructor: { value: MyStream }
 });
+
+/**
+ * options配置流，参数如下：
+ *
+ * highWaterMark  停止读取底层数据源之前的内部缓冲数据的大小
+ * encoding  触发缓冲数据自动编码。可能值包含utf8 和 ascii
+ * objectMode  允许流是一个流对象，而不是字节
+ *
+ * objectMode option 允许流处理JavaScript对象
+ */
